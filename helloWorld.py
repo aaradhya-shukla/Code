f52eb7f63d84a3d40c3ae3a2a67d909755a53350
aaradhya-shukla <138521871+aaradhya-shukla@users.noreply.github.com>
2023-07-04 01:03:44 +0530

    New minor feature

--- helloWorld.py
+++ helloWorld.py
@@ -2,10 +2,15 @@ def main():
   print("Hello world")
   word=input("Enter a sentence")
   print("Word count here is",count(word))
+  print(WhiteSpaceRemover(word))
+  
 def count(word):
   count=0
   for word in words:
     count+=1
   return count
+def WhiteSpaceRemover(word)
+  word.strip()
+  return word
 main()
   
