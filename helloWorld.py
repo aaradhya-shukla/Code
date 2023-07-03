4eda3d194c0e54caa0e71a9a740455311133d528
aaradhya-shukla <138521871+aaradhya-shukla@users.noreply.github.com>
2023-07-04 00:49:35 +0530

    adding word counter in helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,11 @@
 def main():
   print("Hello world")
+  word=input("Enter a sentence")
+  print("Word count here is",count(word))
+def count(word):
+  count=0
+  for word in words:
+    count+=1
+  return count
 main()
+  
